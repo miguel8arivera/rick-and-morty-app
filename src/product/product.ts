4c19d3b6b8995435e026ae096692ec7f21368279
Miguel Angel Ochoa <miguel.angel.8a.rivera@gmail.com>
2023-01-11 20:36:14 -0500

    bug in folders is fixed

---
+++ src/product/product.ts
@@ -0,0 +1,14 @@
+import { Product } from '../types/index';
+
+export const product: Product = {
+  id: 1,
+  codigo: '123',
+  name: 'computer',
+  price: 1000,
+  description: 'computer',
+  image: 'computer.jpg',
+  stock: 10,
+  rating: 4.5,
+  numReviews: 10,
+  isFeatured: true,
+};
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,14 @@
 export const name: string = 'kamila';
+
+export type Product = {
+  id: number;
+  codigo: string;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+  stock: number;
+  rating: number;
+  numReviews: number;
+  isFeatured: boolean;
+};
